d57ce62927d9e74b81305a29b74d31a0369c4576
tnt-haresh <haresh@tneuron.com>
2023-10-08 12:36:28 +0530

    additional exception handler added

--- tntchatbot/router/modelrouter.py
+++ tntchatbot/router/modelrouter.py
@@ -4,8 +4,11 @@
 # This source code's license can be found in the
 # LICENSE file in the root directory of this source tree.
 
-from fastapi import APIRouter, status, File, UploadFile, Header
+import traceback
+
+from fastapi import APIRouter, status, Header
 from fastapi.responses import JSONResponse
+from fastapi import HTTPException
 
 from tntchatbot.schemas import schema
 from tntchatbot.api import api
@@ -20,19 +23,28 @@ async def predict(request: schema.PredictReq, AI_API_KEY: str = Header()):
         response = api.query_model(request, AI_API_KEY)
         logger.debug(f"/predict response {response}")
         return response
+    except HTTPException as e:
+        logger.error(f"Stack trace: {traceback.format_exc()}")
+        raise e
     except Exception as e:
+        logger.error(f"Stack trace: {traceback.format_exc()}")
         return JSONResponse(
             status_code=500,
             content={"detail": f"{str(e)}"})
+        
 
 @router.post("/train", status_code=status.HTTP_201_CREATED, response_model=schema.TrainResp)
 async def train(request: schema.TrainReq, AI_API_KEY: str = Header()):
     try:
-        logger.debug(f"/train request {request}")
+        # logger.debug(f"/train request {request}")
         response = api.train_model(request, AI_API_KEY)
         logger.debug(f"/train response {response}")
         return response
+    except HTTPException as e:
+        logger.error(f"Stack trace: {traceback.format_exc()}")
+        raise e
     except Exception as e:
+        logger.error(f"Stack trace: {traceback.format_exc()}")
         return JSONResponse(
             status_code=500,
             content={"detail": f"{str(e)}"})
@@ -43,7 +55,11 @@ async def predict(request: schema.PredictSheetReq, AI_API_KEY: str = Header()):
         response = api.query_sheet_model(request, AI_API_KEY)
         logger.debug(f"/sheet/predict response {response}")
         return response
+    except HTTPException as e:
+        logger.error(f"Stack trace: {traceback.format_exc()}")
+        raise e
     except Exception as e:
+        logger.error(f"Stack trace: {traceback.format_exc()}")
         return JSONResponse(
             status_code=500,
             content={"detail": f"{str(e)}"})
